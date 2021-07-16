8a36cca887f35b09b3d1a3740ba34434d66b4e42
Letícia Castro <aicitelks@gmail.com>
2021-07-16 19:27:38 -0300

    Concluido com sucesso: Requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,5 +1,3 @@
-const api = require('../src/mockApi');
-
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
 No entanto, nos testes abaixo, queremos que todas as vezes que chamarmos a API a resposta contenha as informações do nosso adminis..Cof! Cof!.. programador favorito, Tunicão.
@@ -21,9 +19,57 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+const api = require('../src/mockApi');
+
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+
+  // api.fetchURL = jest
+  // .fn()
+  // .mockImplementation(async () => {
+  //   return {
+  //     json: async () => {
+  //       return {
+  //         gender: 'male',
+  //         name: {
+  //           first: 'Antônio',
+  //           last: 'Britto'
+  //         },
+  //         location: {
+  //           country: 'Brazil'
+  //         },
+  //         email: 'tunico@bol.com.br',
+  //         login: {
+  //           username: 'tunicao123',
+  //           password: '1234567890'
+  //         }
+  //       }
+  //     }
+  //   }
+  // });
+
+  //apiURL = jest.fn().mockResolvedValue(requestReturn);
+
+  api.fetchURL = jest
+    .fn()
+    .mockImplementation(async () => {
+      return {
+        gender: 'male',
+        name: {
+          first: 'Antônio',
+          last: 'Britto'
+        },
+        location: {
+          country: 'Brazil'
+        },
+        email: 'tunico@bol.com.br',
+        login: {
+          username: 'tunicao123',
+          password: '1234567890'
+        }
+      }
+    });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
